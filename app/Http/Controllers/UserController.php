05e845dee73d637ca5e48291e17411934f784e48
Elerphore <Elerphore@gmail.com>
2023-01-09 17:07:16 +0500

    *

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\UserPost;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function cabinet() {
-        return view('cabinet.index');
+        $likesCount = UserPost::where('user_id', Auth::user()->id)->where('type', 'like')->count();
+        $dislikesCount = UserPost::where('user_id', Auth::user()->id)->where('type', 'dislike')->count();
+        $postsCount = Post::where('user_id', Auth::user()->id)->count();
+
+        return view('cabinet.index')
+                        ->with(compact('likesCount'))
+                        ->with(compact('dislikesCount'))
+                        ->with(compact('postsCount'));
     }
 }
--- resources/views/cabinet/index.blade.php
+++ resources/views/cabinet/index.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.main-layout')
 
 @section('body')
-    Личный кабинет
+    <div class="w-100 mt-4">
+
+        <div class="input-group mb-2">
+            <span class="w-25 input-group-text">Логин и Пароль</span>
+            <input disabled type="text" name="login" id="login" class="form-control" value="{{ Auth::user()->login }}">
+            <input disabled type="password" name="password" id="password" class="form-control" value="{{ Auth::user()->password }}">
+        </div>
+
+        <div class="input-group mb-2">
+            <span class="w-25 input-group-text">Лайки и Дизлайки</span>
+            <input disabled type="text" class="form-control" value="{{ $likesCount }}">
+            <input disabled type="text" class="form-control" value="{{ $dislikesCount }}">
+        </div>
+
+        <div class="input-group mb-2">
+            <span class="w-25 input-group-text">Кол-во созданных постов:</span>
+            <input disabled type="text" class="form-control" value="{{ $postsCount }}">
+        </div>
+
+        <div class="btn-group w-100" role="group" aria-label="Basic example">
+            <a href="{{ route('ownPosts') }}" type="button" class="btn btn-primary">Ваши посты</a>
+            <a href="{{ route('posts') }}" type="button" class="btn btn-primary">Главная</a>
+        </div>
+
+    </div>
 @endsection
